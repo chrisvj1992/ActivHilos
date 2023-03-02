f68db0dfaac7c3e7407ac55ad5fb16836ea8678a
chris <chris@DESKTOP-RJGO0UB>
2023-03-02 11:19:54 -0600

    Agregar archivos de proyecto.

---
+++ Hilos.cs
@@ -0,0 +1,42 @@
+﻿    using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ActivHilos
+{
+    internal class Hilos
+    {
+        int a, i;
+        public Hilos()
+        {
+            Thread t = new Thread(Imprime);
+
+            t.Start();
+
+            Thread t1 = new Thread(Imprime1);
+
+            t1.Start();
+        }
+
+
+
+        public void Imprime()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + "Hilo 1");
+            }
+        }
+
+        public void Imprime1()
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine(i + "Hilo 2");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivHilos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hilos hilos = new Hilos();
+
+            Console.ReadKey();
+        }
+    }
+}
